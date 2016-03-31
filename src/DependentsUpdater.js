b372f94ace4b6a3d0e53f2b6284592f626cabf9b
Artem Vasiliev <artem.job@gmail.com>
2016-03-31 23:52:56 +0300

    fix: main flow works, but with some hardcodes

--- src/DependentsUpdater.js
+++ src/DependentsUpdater.js
@@ -13,7 +13,8 @@ export default class DependentsUpdater {
     let pkg = JSON.parse(fs.readFileSync('./package.json'));
     this.packageName = pkg.name;
     this.packageVersion = pkg.version;
-    this.deps = pkg['semantic-dependents'];
+    this.config = pkg['semantic-dependents-updates'];
+    this.deps = this.config.dependents;
     this.ghToken = env.GH_TOKEN || env.GITHUB_TOKEN;
     this.githubApi = new GitHubApi({
       version: '3.0.0'
@@ -23,8 +24,6 @@ export default class DependentsUpdater {
       token: this.ghToken,
       type: 'oauth'
     });
-
-    this.targetBranch = 'master';
   }
 
   update() {
@@ -36,40 +35,66 @@ export default class DependentsUpdater {
     }
   }
 
-  requestUpdate(packageName, packageVersion, newPkgBody) {
-    let msg = {
-      "message": `chore(package): update ${packageName} to version ${packageVersion}`,
-      "committer": {
-        "name": "semantic-dependents-updates-github bot",
-        "email": "semadep@nowhere.io"
-      },
-      "content": "bXkgdXBkYXRlZCBmaWxlIGNvbnRlbnRz",
-      "sha": "329688480d39049927147c162b9d2deaf885005f"
-    }
+  updateFileInBranch(rawPkg, config) {
+    let msg = Object.assign(this.gitRepoOptions(config), {
+      path: PACKAGE_JSON,
+      branch: config.newBranch,
+      message: `chore(package): update ${this.packageName} to version ${this.packageVersion}`,
+      author: Object.assign({
+            name: 'semantic-dependents-updates-github bot',
+            email: 'semadep@nowhere.io'
+          }, this.config.author || {}),
+      sha: config.oldPackageSha
+    });
+    let msgWithFile = Object.assign({
+      content: 'bXkgdXBkYXRlZCBmaWxlIGNvbnRlbnRz'
+    }, msg);
+    return new Promise((resolve) => {
+      this.githubApi.repos.updateFile(msgWithFile, (err, data) => {
+        if (err) {
+          throw new Error(`Couldn't commit a change  ${JSON.stringify(msg)} for ${config.targetPackageName}: ${err}`);
+        }
+        resolve();
+      });
+    });
   }
 
-  createPullRequest(packageName, packageVersion, newPkgBody) {
+  createPullRequest(config) {
+    let msg = Object.assign(this.gitRepoOptions(config), {
+      title: `Update ${this.packageName} to version ${this.packageVersion}`,
+      base: config.targetBranch,
+      head: config.newBranch
+    });
+    return new Promise((resolve) => {
+      this.githubApi.pullRequests.create(msg, (err, data) => {
+        if (err) {
+          throw new Error(`Couldn't create a PR for ${config.targetPackageName}: ${err}`);
+        }
+        resolve();
+      });
+    });
+  }
+
+  createBranch(config) {
     let random = `${Date.now()}`;
-    let branchName = `${this.branchNameBase}-${packageVersion}-${random}`;
-    let sha = 'a961ce930b0570bdf3c7e9dbeaa3abe198ab9150';
-    let msg = {
-      user: this.gitRepoOwner,
-      owner: this.gitRepoOwner,
-      repo: this.gitRepo,
-      ref: `refs/heads/${branchName}`,
-      sha: sha
-    };
-    this.githubApi.gitdata.createReference(msg, (err, data) => {
-      if (err) {
-        throw new Error(`Couldn't create a new branch from sha ${sha}: ${err}`);
-      }
-      this.requestUpdate(packageName, packageVersion, newPkgBody)
+    config.newBranch = `${config.branchNameBase}-${this.packageVersion}-${random}`;
+    let currentHeadSha = 'a961ce930b0570bdf3c7e9dbeaa3abe198ab9150';
+    let msg = Object.assign(this.gitRepoOptions(config), {
+      ref: `refs/heads/${config.newBranch}`,
+      sha: currentHeadSha
+    });
+    return new Promise((resolve) => {
+      this.githubApi.gitdata.createReference(msg, (err, data) => {
+        if (err) {
+          throw new Error(`Couldn't create a new branch for ${config.targetPackageName} from sha ${currentHeadSha}: ${err}`);
+        }
+        resolve();
+      });
     });
 
   }
 
-  processTargetPackageJson(err, data) {
-    let rawPkg = data;
+  processTargetPackageJson(rawPkg, config) {
     let pkg = JSON.parse(rawPkg, 'utf8');
     let key = ['dependencies', 'devDependencies', 'peerDependencies'].find((k) => {
       return (pkg[k] || {})[this.packageName];
@@ -80,31 +105,61 @@ export default class DependentsUpdater {
     }
     let currentVersion = pkg[key][this.packageName];
     if (currentVersion !== this.packageVersion) {
-      console.log(`Updating ${this.packageName} version from ${currentVersion} to ${this.packageVersion}`);
+      console.log(`Updating ${this.packageName} version at ${config.targetPackageName} from ${currentVersion} to ` +
+          this.packageVersion);
+
       rawPkg = rawPkg.replace(`"${this.packageName}": "${currentVersion}"`,
           `"${this.packageName}": "${this.packageVersion}"`);
-      this.createPullRequest(this.packageName, this.packageVersion, rawPkg);
+      this.createBranch(config).then(() => {
+        return this.updateFileInBranch(rawPkg, config);
+      }).then(() => {
+        this.createPullRequest(config).then(() => console.log(`Created a PR for ${config.targetPackageName}`));
+      });
     } else {
       console.log(`This package already have ${this.packageName} at version ${this.packageVersion}`);
       return;
     }
   }
 
+  gitRepoOptions(config) {
+    return {
+      user: config.gitRepoOwner,
+      repo: config.gitRepo
+    };
+  }
+
+  getTargetPackageJson(options, config) {
+    return new Promise((resolve) => {
+      this.githubApi.repos.getContent(Object.assign(this.gitRepoOptions(config), {
+            ref: config.targetBranch,
+            path: PACKAGE_JSON
+          }, options), (err, data) => {
+            if (err) {
+              throw new Error(`Couldn't get ${PACKAGE_JSON} of ${config.targetPackageName}: ${err}`);
+            }
+            resolve(data);
+          }
+      );
+    });
+  }
+
   updateDependency(dep, info) {
     let gitUrl = info.repo;
-    this.branchNameBase = info.branchNameBase;
+    let config = {};
+    config.targetPackageName = dep;
+    config.branchNameBase = info.branchNameBase;
+    config.targetBranch = info.targetBranch || 'master';
     console.log(`Trying to update dependent package ${dep} at ${gitUrl}`);
     let [owner, repo] = parseSlug(gitUrl);
-    this.gitRepo = repo;
-    this.gitRepoOwner = owner;
-    this.githubApi.repos.getContent({
-          user: this.gitRepoOwner,
-          repo: this.gitRepo,
-          ref: this.targetBranch,
-          path: PACKAGE_JSON,
-          headers: {Accept: 'application/vnd.github.v3.raw'}
-        },
-        this.processTargetPackageJson.bind(this));
+    config.gitRepo = repo;
+    config.gitRepoOwner = owner;
+    this.getTargetPackageJson({}, config).then((data) => {
+      config.oldPackageSha = data.sha;
+    }).then(() => {
+      this.getTargetPackageJson({headers: {Accept: 'application/vnd.github.v3.raw'}}, config).then((data) => {
+        this.processTargetPackageJson(data, config);
+      });
+    });
   }
 
   run() {
